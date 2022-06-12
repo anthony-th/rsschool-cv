e4b5c662bffef87f6894f20d79cf0672216cf046
Anthony-th <yefremov.anthony@gmail.com>
2022-06-12 06:43:52 +0300

    refactor: change button-top

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,15 @@ const navList = document.querySelector('.header__nav-list');
 const links = document.querySelectorAll('.header__nav-list-link');
 const body = document.querySelector('body');
 const shadow = document.querySelector('.shadow-block');
+const btnTop = document.querySelector('.button-top');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+});
 
 const changeMenu = function() {
   navList.classList.toggle('open');
